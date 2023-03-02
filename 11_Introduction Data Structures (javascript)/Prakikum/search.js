40d78afe82c220efcfc74c9045baa0e798398f0c
eldavina <eldaaavina@gmail.com>
2023-03-03 00:03:37 +0700

    Data Structure

--- 11_Introduction Data Structures (javascript)/Prakikum/search.js
+++ 11_Introduction Data Structures (javascript)/Prakikum/search.js
@@ -1,29 +1,14 @@
-function search() {
-    //     if(namaproduct==namaproduct){
-    //         document.getElementById('namaproduk').value
-    //     }
-    // }
-    // // document.getElementById("namaproduct").innerHTML = item;
-    // if(item.namaproduk.value>-1){
-    //     return alert(`${namaproduk} tersedia`)
-    // }
-    // else{
-    //     return alert(`${namaproduk} tidak tersedia`)
-    // }
-//   }
-
-// document.getElementById(cari).value
-// document.getElementById('namaproduct').value = value.namaproduct
-
-var cari = document.getElementById("cari").value;
-// var namaproduct = document.getElementById('namaproduk').value;
-document.getElementById('namaproduk').value = value.namaproduk
-
-// if(cari.value == namaproduct.value){
-    if(cari.value(save(namaproduk))==true){
-    return alert(`${cari} tersedia`)
-}
-    else{
-        return alert(`${namaproduk} tidak tersedia`)
+function search(){
+        let mencari = false
+        const search = document.getElementById('cari')
+        contactList.forEach((data) => {
+            if (data[1] === search.value) {
+                return mencari = true
+            }
+        })
+        if (mencari) {
+            alert('Data Ada')
+        } else {
+            alert('Data tidak ada')
+        }
     }
-}
